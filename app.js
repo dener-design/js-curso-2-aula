683d626a1ede12b575c62fdb3f07cb165b580120
Dener Patrick <dener@outlook.com>
2024-01-21 22:36:42 -0300

    Base da aula 2

---
+++ app.js
@@ -0,0 +1,9 @@
+let tituloPrincipal = document.querySelector('h1');
+tituloPrincipal.innerHTML = 'Jogo do número secreto';
+
+let paragrafoPrincipal = document.querySelector('p');
+paragrafoPrincipal.innerHTML = 'Escolha um número entre 1 a 10';
+
+function verificarChute(){
+    console.log('O Botão foi clicado');
+}
